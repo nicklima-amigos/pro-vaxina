8b773a4c0125031458fbfddef11a51e162c4a324
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-26 18:59:47 -0300

    refactor: read app port from .env file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PatientsModule } from './patients/patients.module';
 import { VaccinationRecordsModule } from './vaccination-records/vaccination-records.module';
 import { VaccinesModule } from './vaccines/vaccines.module';
 
 @Module({
-  imports: [PatientsModule, VaccinationRecordsModule, VaccinesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PatientsModule,
+    VaccinationRecordsModule,
+    VaccinesModule,
+  ],
 })
 export class AppModule {}
