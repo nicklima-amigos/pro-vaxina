18aebc5c4f4bbc856dd80ec51877d73d6212a785
Caio Lucius <caiolucius2010@hotmail.com>
2024-03-27 14:12:05 -0300

    chore: adjust some settings

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -14,9 +14,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+return (<html lang="en">
+  <body className={inter.className}>{children}</body></html>
+);
 }
