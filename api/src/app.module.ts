12f1cbaf399243d97c6f25600b99acd291fe0f1f
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-27 09:00:05 -0300

    chore: synchronize database when environment is not production

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PatientsModule } from './patients/patients.module';
 import { VaccinationRecordsModule } from './vaccination-records/vaccination-records.module';
 import { VaccinesModule } from './vaccines/vaccines.module';
@@ -8,9 +8,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'better-sqlite3',
-      database: 'db.sqlite3',
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const nodeEnv = configService.get<string>('NODE_ENV');
+        return {
+          type: 'better-sqlite3',
+          database: 'db.sqlite3',
+          synchronize: nodeEnv !== 'production',
+        };
+      },
     }),
     PatientsModule,
     VaccinationRecordsModule,
