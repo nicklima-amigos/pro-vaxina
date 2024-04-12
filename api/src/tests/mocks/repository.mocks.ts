b5dd58069894bd572d94d44a9a1b4c36f13e0600
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-04-12 12:38:54 -0300

    refactor: use mock repository factories to avoid repeating code

--- api/src/tests/mocks/repository.mocks.ts
+++ api/src/tests/mocks/repository.mocks.ts
@@ -4,29 +4,19 @@ import { Patient } from '@src/patients/entities/patient.entity';
 import { VaccinationRecord } from '@src/vaccination-records/entities/vaccination-record.entity';
 import { Vaccine } from '@src/vaccines/entities/vaccine.entity';
 
-export const vaccinationRecords = {
+export const repositoryMockFactory = () => ({
   find: jest.fn(),
   findOne: jest.fn(),
   save: jest.fn(),
   update: jest.fn(),
   delete: jest.fn(),
-};
+});
 
-export const patients = {
-  find: jest.fn(),
-  findOne: jest.fn(),
-  save: jest.fn(),
-  update: jest.fn(),
-  delete: jest.fn(),
-};
+export const vaccinationRecords = repositoryMockFactory();
 
-export const vaccines = {
-  find: jest.fn(),
-  findOne: jest.fn(),
-  save: jest.fn(),
-  update: jest.fn(),
-  delete: jest.fn(),
-};
+export const patients = repositoryMockFactory();
+
+export const vaccines = repositoryMockFactory();
 
 export const providers: Provider[] = [
   {
