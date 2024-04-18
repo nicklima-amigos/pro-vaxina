eb507b69e2c6b7d87f58e0abc491b34f5f5b6f6a
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-04-17 22:42:18 -0300

    feat: created system flow adding vaccines, patients and home pages

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Inter as FontSans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import '../styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Pro Vaxina',
 };
-
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-gray-200 antialiased',
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
